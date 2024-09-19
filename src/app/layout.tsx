8cbd0c7f5b6e77520a4f99ac51767f26e2727d74
Giliarney <giliarneymario@gmail.com>
2024-09-19 03:03:50 -0300

    deploy site 8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@
 import { Heebo } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 const queryClient = new QueryClient();
@@ -18,9 +17,7 @@ export default function RootLayout({ children }: Readonly<{ children: ReactNode 
         className={cn("min-h-screen bg-background font-serif antialiased", heebo.className)}
       >
         <QueryClientProvider client={queryClient}>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
             {children}
-          </ThemeProvider>
         </QueryClientProvider>
       </body>
     </html>
