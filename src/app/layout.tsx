4203ec60bd7d0996278c26474159dd3bd15f0299
Giliarney <giliarneymario@gmail.com>
2024-09-19 03:10:04 -0300

    deploy site 9

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import { Heebo } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
@@ -12,7 +11,11 @@ const heebo = Heebo({weight:'500',  subsets: ["latin"]} );
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
 
   return (
-    <html lang="pt-br">
+    <html lang="pt-br"  
+    >
+      <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="https://i.imgur.com/MKiQNpb.png"></link>
+      </head>
       <body
         className={cn("min-h-screen bg-background font-serif antialiased", heebo.className)}
       >
